3cde42abdb678fc564cf4c305b6c94a5593a6e41
Kevin Pan <kevin900@hotmail.ca>
2017-11-24 17:04:28 -0500

    support gravity

--- vector2d.js
+++ vector2d.js
@@ -4,6 +4,11 @@ class Vector{
     this.y=y;
   }
 
+  set(v){
+    this.x=v.x;
+    this.y=v.y;
+  }
+
   add(v){
     this.x+=v.x;
     this.y+=v.y;
@@ -14,7 +19,26 @@ class Vector{
     this.y-=v.y;
   }
 
+  scale(v){
+    this.x*=v.x;
+    this.y*=v.y;
+  }
+
   magnitude(){
     return Math.sqrt(this.x*this.x+this.y*this.y);
+  }
+
+  // radians
+  angle(){
+    if(this.y==0)
+      return 0;
+    else if(this.x>0&&this.y>0)
+      return Math.atan(this.y/this.x);
+    else if(this.x<0&&this.y>0)
+      return Math.PI+Math.atan(this.y/this.x);
+    else if(this.x<0&&this.y<0)
+      return Math.PI+Math.atan(this.y/this.x);
+    else
+      return 2*Math.PI+Math.atan(this.y/this.x);
   }
 }
